e8ebc0a7b03f341fce7a1b24a9f02c29f7831f20
barak353 <barak353@gmail.com>
2017-06-07 22:47:04 +0300

    updated teacher FillFinalEvaluation.

--- Prototype-client/src/Teacher/FillFinalEvaluationController.java
+++ Prototype-client/src/Teacher/FillFinalEvaluationController.java
@@ -2,11 +2,14 @@ package Teacher;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import Login.LoginController;
 import Entity.User;
 import application.QueryController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -54,10 +57,14 @@ public class FillFinalEvaluationController extends QueryController implements In
     private Button back;
 
     @FXML
-    private ComboBox CourseList;
+    private ComboBox<String> CourseList;
 
     @FXML
     private Text userID;
+    
+
+    @FXML
+    private Text textMSG;
 	
     @FXML
     void TurningBack(ActionEvent event) {
@@ -65,25 +72,49 @@ public class FillFinalEvaluationController extends QueryController implements In
     	this.Back("/Teacher/TeacherMain.fxml",nextController, event);
     }
     
+    @FXML
     void saveB(ActionEvent event) {
-    	try {
-			    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Teacher/message.fxml"));
-		        loader.setController(new messageController("messageController"));
-		        Pane login_screen_parent = loader.load();
-				Scene login_screen_scene=new Scene(login_screen_parent);
-				Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//the scene that the event came from.
-				//app_stage.hide();
-				app_stage.setScene(login_screen_scene);
-				app_stage.show(); 
-				} catch (IOException e) {
-					System.err.println("Missing message.fxml file");
-					e.printStackTrace();
-				}
+    	textMSG.setVisible(true);
+    	String finalGrade = grade.getText();
+    	int fnlGrade;
+    	try{
+    		fnlGrade = Integer.parseInt(finalGrade);
+    	}catch(NumberFormatException e){
+    		textMSG.setText("Final grade most contain only numbers.");
+    		return;
+    	}
+    	if(fnlGrade < 100 && fnlGrade < 0){
+    		textMSG.setText("Final grade most be between 100 to 0."); return;
+    	}
+    	
+    	
     }
     public void initialize(URL arg0, ResourceBundle arg1) {//this method perform when this controller scene is showing up.
     	User user = User.getCurrentLoggedIn();
     	userID.setText(user.GetUserName());
+    	String teacherID = user.GetID();
+    	ArrayList<ArrayList<String>> res = (ArrayList<ArrayList<String>>) transfferQueryToServer("SELECT cI FROM teacherincourse WHERE TeacherID="+teacherID);
+    	ArrayList<String> courseNameList = new ArrayList<String>();
+    	ArrayList<ArrayList<String>> res2;
+    	for(ArrayList<String> row:res){
+        	res2 = (ArrayList<ArrayList<String>>) transfferQueryToServer("SELECT courseName,idcourses FROM courses WHERE idcourses="+row.get(0));
+    		courseNameList.add(res2.get(0).get(0)+"("+res2.get(0).get(1)+")");
+    	}
+    	System.out.println("courseNameList: "+courseNameList);
+    	
+	    ObservableList obList= FXCollections.observableList(courseNameList);;
+	    CourseList.setItems(obList);
     }
+    
+    @FXML
+    void chooseCourse(ActionEvent event) {
+    	String chooseCourse = CourseList.getValue();
+    	String idcourses = chooseCourse.substring(chooseCourse.indexOf("(") + 1, chooseCourse.indexOf(")"));//get the idcourses that is inside a ( ).
+    	ArrayList<ArrayList<String>> res = (ArrayList<ArrayList<String>>) transfferQueryToServer("SELECT studentID FROM studentincourse WHERE idcourses="+idcourses);
+    	ObservableList obList= FXCollections.observableList(res);;
+    	StudentList.setItems(obList);
+    }
+    
     @FXML
     void LogOut(ActionEvent event) {
 		 try 
--- Prototype-client/src/Teacher/TeacherMainController.java
+++ Prototype-client/src/Teacher/TeacherMainController.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -35,7 +36,7 @@ public class TeacherMainController extends QueryController implements Initializa
 	
 	    @FXML
 	    private Button logout;
-
+	    
 	    @FXML
 	    private Button FillFinalEvaluation;
 
