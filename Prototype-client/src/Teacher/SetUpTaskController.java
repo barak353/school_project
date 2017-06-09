f849ca47db1482f6568155bc961eef49e461e28f
חן <@henhak>
2017-06-09 11:26:48 +0300

    update teacher - set task &upload task

--- Prototype-client/src/Teacher/SetUpTaskController.java
+++ Prototype-client/src/Teacher/SetUpTaskController.java
@@ -2,11 +2,14 @@ package Teacher;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.scene.control.ComboBox;
 import Login.LoginController;
 import Entity.User;
 import application.QueryController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -45,6 +48,12 @@ public class SetUpTaskController extends QueryController implements Initializabl
     @FXML
     private Text userID;
     
+   
+    
+    @FXML
+    private ComboBox<String> CourseList;
+    
+   
   //-----------------------------------------------------------//
 
     @FXML
@@ -54,26 +63,48 @@ public class SetUpTaskController extends QueryController implements Initializabl
     }
 
     @FXML
-    void UploadTask(ActionEvent event) {
+    void Continue(ActionEvent event) {
     	try {
-			    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Teacher/UploadTask.fxml"));
-		        loader.setController(new UploadTaskController("UploadTaskController"));
+    	  		    		
+    			String chooseCourse = CourseList.getValue();
+        		String idcourses = chooseCourse.substring(chooseCourse.indexOf("(") + 1, chooseCourse.indexOf(")"));//get the idcourses that is inside a ( ).
+        		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Teacher/UploadTask.fxml"));
+        		UploadTaskController controller = new UploadTaskController("UploadTaskController");
+        		controller.setCourseN(chooseCourse);
+		        loader.setController(controller);
 		        Pane login_screen_parent = loader.load();
 				Scene login_screen_scene=new Scene(login_screen_parent);
 				Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//the scene that the event came from.
 				app_stage.hide();
 				app_stage.setScene(login_screen_scene);
 				app_stage.show(); 
+				
 				} catch (IOException e) {
 					System.err.println("Missing UploadTask.fxml file");
 					e.printStackTrace();
 				}
     }
     
+    
     public void initialize(URL arg0, ResourceBundle arg1) {//this method perform when this controller scene is showing up.
     	User user = User.getCurrentLoggedIn();
     	userID.setText(user.GetUserName());
+        
+    	String teacherID = user.GetID();
+    	ArrayList<ArrayList<String>> res = (ArrayList<ArrayList<String>>) transfferQueryToServer("SELECT cI FROM teacherincourse WHERE TeacherID="+teacherID);
+    	ArrayList<String> courseNameList = new ArrayList<String>();
+    	ArrayList<ArrayList<String>> res2;
+    	for(ArrayList<String> row:res){
+        	res2 = (ArrayList<ArrayList<String>>) transfferQueryToServer("SELECT courseName,idcourses FROM courses WHERE idcourses="+row.get(0));
+    		courseNameList.add(res2.get(0).get(0)+"("+res2.get(0).get(1)+")");
+    	}
+    	System.out.println("courseNameList: "+courseNameList);
+    	
+	    ObservableList obList= FXCollections.observableList(courseNameList);;
+	    CourseList.setItems(obList);
     }
+
+  /*----------------------------------------------------------------------------------------------------------------*/  
     @FXML
     void LogOut(ActionEvent event) {
 		 try 
@@ -93,6 +124,6 @@ public class SetUpTaskController extends QueryController implements Initializabl
 				e.printStackTrace();
 				}
     }
-
     
+
 }
