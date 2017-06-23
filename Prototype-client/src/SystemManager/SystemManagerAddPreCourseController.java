703fe2a00f673c21367c3b63b3b42d1ffc4bca85
נופר <@192.168.56.1>
2017-06-23 13:26:17 +0300

    nofar e

--- Prototype-client/src/SystemManager/SystemManagerAddPreCourseController.java
+++ Prototype-client/src/SystemManager/SystemManagerAddPreCourseController.java
@@ -117,6 +117,29 @@ public class SystemManagerAddPreCourseController extends QueryController impleme
 	    	}
     	}
 	    //PreCourses.clear();
+    	
+    	
+    	
+		try {
+	    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/SystemManager/SystemManagerAddPreCourseWindow.fxml"));
+	    	SystemManagerAddPreCourseController controller = new SystemManagerAddPreCourseController("SystemManagerAddPreCourseController");
+	    	controller.setCourseID(lastCourse);
+		    loader.setController(controller);
+			Pane login_screen_parent = loader.load();
+			Scene login_screen_scene=new Scene(login_screen_parent);
+			Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();//the scene that the event came from.
+			app_stage.hide();
+			app_stage.setScene(login_screen_scene);
+			app_stage.show(); 
+		} 
+    	catch (IOException e) {//problem with the teacherWindow.xml file.
+			System.err.println("Missing SystemManagerAddPreCourseWindow.fxml file");
+			e.printStackTrace();
+		}
+
+		
+		
+		
     } 
     
 	//------------------------------------------// 
